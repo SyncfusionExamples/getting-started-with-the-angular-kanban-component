ed84ecd1a7fff20d1360101d4d9c7facef9e1586
Ammu1492 <60737792+Ammu1492@users.noreply.github.com>
2023-04-28 23:03:46 +0530

    added asample

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense("ORg4AjUWIQA/Gnt2VFhhQlJBfVtdVXxLflF1VWBTflx6d1xWESFaRnZdQV1gS3lTfkZlXXpWdndT");
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
